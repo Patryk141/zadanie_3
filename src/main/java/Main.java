c3f3d8dd1ad7f0651898522a56beeef90c9afc3e
Patryk Piskorski <268487@student.pwr.edu.pl>
2022-10-16 11:21:19 +0200

    Second version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
         Book book = new Book("Tolkien","The lord of the rings");
-        Gson gson = new Gson();
         Book book1 = new Book("J.K. Rowling","Harry Potter");
         Book book2 = new Book("Stephen King","It");
+        Gson gson = new Gson();
         String bookJson = gson.toJson(book);
         String book1Json = gson.toJson(book1);
         String book2Json = gson.toJson(book2);
